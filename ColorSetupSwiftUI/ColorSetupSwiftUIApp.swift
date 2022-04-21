b8c82830922b7a6ae2b1eed8d953c95984d4d602
seregious <seregious@gmail.com>
2022-04-21 17:38:58 +0300

    Initial Commit

---
+++ ColorSetupSwiftUI/ColorSetupSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSetupSwiftUIApp.swift
+//  ColorSetupSwiftUI
+//
+//  Created by Сергей Иванчихин on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSetupSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
